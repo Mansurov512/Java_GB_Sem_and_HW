9bc1108f62bb46ba4d91d9c2da6e35382d820967
Mansurov Mikhail <do9jng@gmail.com>
2023-01-23 19:20:40 +0300

    Разобрался со структурой

---
+++ src/Homeworks/HW01/Task01.java
@@ -0,0 +1,11 @@
+package Homeworks.HW01;
+
+public class Task01 {
+
+        public static void main(String[] args) {
+
+            System.out.println("Hello world!!!");
+            System.out.println("Test!!!88989");
+        }
+    }
+
---
+++ src/Homeworks/HW02/Task01.java
@@ -0,0 +1,11 @@
+package Homeworks.HW02;
+
+public class Task01 {
+
+        public static void main(String[] args) {
+
+            System.out.println("Hello world!!!");
+            System.out.println("Test!!!88989");
+        }
+    }
+
