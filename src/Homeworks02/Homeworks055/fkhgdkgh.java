cbc417a196f98499cb1784a4d6f449d5840f5dd9
Mansurov Mikhail <do9jng@gmail.com>
2023-01-23 19:20:40 +0300

    Разобрался со структурой

--- src/Homeworks02/Homeworks055/fkhgdkgh.java
+++
@@ -1,9 +0,0 @@
-package Homeworks02.Homeworks055;
-
-public class fkhgdkgh {
-    public static void main(String[] args) {
-
-        System.out.println("Hello world!!!");
-        System.out.println("Test!!!111232131");
-    }
-}
--- src/Homeworks02/Test02.java
+++
@@ -1,4 +0,0 @@
-package Homeworks02;
-
-public class Test02 {
-}
