9012fdb9c4c614ef4b0475c015e3c09bf9cda008
Mansurov Mikhail <do9jng@gmail.com>
2023-01-30 18:34:37 +0300

    Сделал 1ую задачу 2ого ДЗ

--- src/Homeworks/HW02/Task01.java
+++ src/Homeworks/HW02/Task01.java
@@ -1,11 +1,42 @@
 package Homeworks.HW02;
+import java.io.IOException;
+import java.util.logging.*;
 
-public class Task01 {
+public class Task01
+{
+    public static void main(String[] args) throws IOException // почему-то IDE попросила добавить throws IOException, когда
+                                                              // я указывал FileHandler, видимо на случай ошибок при работе с файлом
+    {
+        Logger logger = Logger.getLogger(Task01.class.getName());
+        FileHandler fh = new FileHandler("src/Homeworks/HW02/logTask01.txt");
+        logger.addHandler(fh);
+        SimpleFormatter sFormat = new SimpleFormatter();
+        fh.setFormatter(sFormat);
 
-        public static void main(String[] args) {
+        int arrayNumbers[] = new int[]{3, 1, 8, 9, 5, 7, 4, 6, 2};
 
-            System.out.println("Hello world!!!");
-            System.out.println("Test!!!sadadad");
+        for (int i = 0; i < arrayNumbers.length; i++)
+        {
+            for (int j = 1; j < arrayNumbers.length - i; j++)
+            {
+                int temp = arrayNumbers[j];
+                logger.info("Сравниваем " + arrayNumbers[j-1] + " и " + arrayNumbers[j]);
+
+                if (arrayNumbers[j-1] > arrayNumbers[j])
+                {
+                    logger.info("Меняем местами " + arrayNumbers[j-1] + " и " + arrayNumbers[j]);
+                    arrayNumbers[j] = arrayNumbers[j-1];
+                    arrayNumbers[j-1] = temp;
+                }
+                String str = "[";
+                for (int k = 0; k < arrayNumbers.length; k++)
+                {
+                    str = str + arrayNumbers[k] + " ";
+                }
+                str = str.trim() + "]";
+                logger.info("Массив на данной итерации: " + str);
+            }
         }
     }
+}
 
---
+++ src/Homeworks/HW02/Task02.java
@@ -0,0 +1,51 @@
+// 03 Реализовать простой калькулятор
+
+package Homeworks.HW02;
+
+import java.util.Scanner;
+public class Task02
+{
+    public static void main(String[] args)
+    {
+        System.out.println(Calculator());
+    }
+    public static double Calculator()
+    {
+        double result = 0;
+
+        Scanner Calc = new Scanner(System.in);
+        System.out.print("Введите цифрами действие которое хотите сделать:\n" +
+                "1 - + - сложить;\n" +
+                "2 - - - вычесть;\n" +
+                "3 - * - умножить;\n" +
+                "4 - / - поделить;\n");
+
+        int action = Calc.nextInt();
+
+        System.out.print("Введите первое число: ");
+        double num01 = Calc.nextDouble();
+
+        System.out.print("Введите второе число: ");
+        double num02 = Calc.nextDouble();
+
+        switch(action){
+            case 1:
+                result = num01 + num02;
+                break;
+            case 2:
+                result = num01 - num02;
+                break;
+            case 3:
+                result = num01 * num02;
+                break;
+            case 4:
+                result = num01 / num02;
+                break;
+            default:
+                result = -1; //при некорректном вводе действия
+        }
+        return result;
+
+        //Calc.close();
+    }
+}
