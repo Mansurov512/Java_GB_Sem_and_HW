bd62f9170f3490c43d1f261bb062a40b028543ce
Mansurov Mikhail <do9jng@gmail.com>
2023-01-30 18:59:24 +0300

    Закончил 2ое ДЗ.

--- src/Homeworks/HW02/Task02.java
+++ src/Homeworks/HW02/Task02.java
@@ -2,50 +2,63 @@
 
 package Homeworks.HW02;
 
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.logging.*;
+
 public class Task02
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
+
     {
         System.out.println(Calculator());
     }
-    public static double Calculator()
+    public static double Calculator() throws IOException
     {
+        Logger logger = Logger.getLogger(Task01.class.getName());
+        FileHandler fh = new FileHandler("src/Homeworks/HW02/logTask02.txt");
+        logger.addHandler(fh);
+        SimpleFormatter sFormat = new SimpleFormatter();
+        fh.setFormatter(sFormat);
+
         double result = 0;
 
         Scanner Calc = new Scanner(System.in);
-        System.out.print("Введите цифрами действие которое хотите сделать:\n" +
-                "1 - + - сложить;\n" +
-                "2 - - - вычесть;\n" +
-                "3 - * - умножить;\n" +
-                "4 - / - поделить;\n");
+        System.out.print("Введите действие которое хотите сделать:\n" +
+                "+ - сложить;\n" +
+                "- - вычесть;\n" +
+                "* - умножить;\n" +
+                "/ - поделить;\n");
 
-        int action = Calc.nextInt();
+        String action = Calc.nextLine();
+        logger.info("Пользователь ввёл " + action);
 
-        System.out.print("Введите первое число: ");
+        System.out.print("Введите первое число: \n");
         double num01 = Calc.nextDouble();
+        logger.info("Пользователь ввёл " + num01);
 
         System.out.print("Введите второе число: ");
         double num02 = Calc.nextDouble();
+        logger.info("Пользователь ввёл " + num02);
 
         switch(action){
-            case 1:
+            case "+":
                 result = num01 + num02;
                 break;
-            case 2:
+            case "-":
                 result = num01 - num02;
                 break;
-            case 3:
+            case "*":
                 result = num01 * num02;
                 break;
-            case 4:
+            case "/":
                 result = num01 / num02;
                 break;
             default:
+                logger.log(Level.WARNING, "Пользователь ввёл некорректное " + action);
                 result = -1; //при некорректном вводе действия
         }
+        logger.info("Результат равен " + result);
         return result;
-
-        //Calc.close();
     }
 }
